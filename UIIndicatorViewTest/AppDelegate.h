75e5491ad2e5d98b0b6b6c7a9f002a9a3c94bf5a
kejianfang <kejianfang@meituan.com>
2018-05-16 15:59:04 +0800

    init

---
+++ UIIndicatorViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIIndicatorViewTest
+//
+//  Created by 柯建芳 on 2018/5/16.
+//  Copyright © 2018年 柯建芳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
